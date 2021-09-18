1cc43346938a802d9ef21a22a734564993795dcd
AmigoDroid <coltman05@gmail.com>
2021-09-18 17:44:10 -0300

    correçao no servidor

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 const express = require('express');
 const morgan = require('morgan');
 const cors = require('cors');
-const boryParser = require('body-parser');
+const boryparser = require('body-parser');
 //const bodyParser = require('body-parser');
 
 const app = express();
 app.use(morgan('dev'));
-app.use(bodyParser.urlencoded({extended:false}));
-app.use(express.json);
+app.use(boryparser.urlencoded({extended:false}));
+app.use(express.json());
 app.use(cors());
+
+app.listen(2000,()=>{
+    console.log('rodando na porta:2000');
+})
