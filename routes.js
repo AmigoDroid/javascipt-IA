e4740d4751b9173ec21f3a3a0e7ad765443befdf
AmigoDroid <coltman05@gmail.com>
2021-09-19 20:37:01 -0300

    update-json

--- routes.js
+++ routes.js
@@ -3,9 +3,9 @@ const routes = express.Router();
 
 
 let usuarios =[
-        {id:'0', Nome:'Luciano',SobreNome:'da silva costa',Cpf:0,Saldo:54280.78,Logado:true,usuariro:'admin',senha:0},
-        {id:'1',Nome:'Larissa',SobreNome:'gomes lima',Cpf:0,Saldo:54280.78,Logado:true,usuariro:'esposa',senha:0},
-        {id:'2',Nome:'Luiz Gustavo',SobreNome:'lima costa',Cpf:0,Saldo:54280.78,Logado:true,usuariro:'filho',senha:0}
+        {id:'0', Nome:'Luciano',SobreNome:'da silva costa',Cpf:0,Saldo:54280.78,usuariro:'admin',senha:0},
+        {id:'1',Nome:'Larissa',SobreNome:'gomes lima',Cpf:0,Saldo:10000000,usuariro:'Larissa123',senha:5824},
+        {id:'2',Nome:'Luiz Gustavo',SobreNome:'lima costa',Cpf:0,Saldo:54450280.78,usuariro:'Gustavo123',senha:5824}
 ]
 let usuario={
     Nome:'Luciano',Idade:'22',SobreNome:'da silva costa',Saldo:293752.13,Logado:true
