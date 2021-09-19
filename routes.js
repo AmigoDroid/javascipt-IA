12c5b7c6a275bc273bcbfe4b98584e1bdc35814d
AmigoDroid <coltman05@gmail.com>
2021-09-19 15:49:43 -0300

    user-nodemon-server

--- routes.js
+++ routes.js
@@ -2,14 +2,21 @@ const express = require('express');
 const routes = express.Router();
 
 
-let db ={
-        Esposa:'Larissa',IdadeEsposa:'16',
-        filho:'Gustavo',IdadeFilho:'2'}
+let usuarios ={
+    '1':{Nome:'Luciano',SobreNome:'da silva costa',Cpf:0,Saldo:54280.78,Logado:true,usuariro:'admin',senha:0}
+}
 
 let usuario={
     Nome:'Luciano',Idade:'22',SobreNome:'da silva costa',Saldo:293752.13,Logado:true
 }
+function teste(user,pass){
+    
+}
 
+
+
+
+//ROTAS DO MEU SERVIDOR
  routes.get('/',(req,res) =>{
     return res.end('<h1>ERRO GET 404!</h1>')
 })
@@ -17,8 +24,8 @@ let usuario={
 routes.get('/api',(req,res) => {
     return res.json(db);
 });
-routes.get('/usuario',(req,res)=>{
-    return res.json(usuario)  
+routes.get('/api/usuario',(req,res)=>{
+    return res.json(usuarios);
 })
 routes.post('/adduser',(req,res)=>{
     const dados = req.body;
@@ -26,6 +33,9 @@ routes.post('/adduser',(req,res)=>{
         return res.status(400).end();
     }
 })
+routes.delete('/usuario/del=:id',(req,res)=>{
+    const id = req.params.id;
+});
 
 
 
