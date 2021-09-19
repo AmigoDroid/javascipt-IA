5c653124e524e427fff3cbbb3a3cc2835d3f64bc
AmigoDroid <coltman05@gmail.com>
2021-09-19 16:53:10 -0300

    update-usuarios

--- routes.js
+++ routes.js
@@ -3,7 +3,11 @@ const routes = express.Router();
 
 
 let usuarios ={
-    '1':{Nome:'Luciano',SobreNome:'da silva costa',Cpf:0,Saldo:54280.78,Logado:true,usuariro:'admin',senha:0}
+    'user':[
+        {Nome:'Luciano',SobreNome:'da silva costa',Cpf:0,Saldo:54280.78,Logado:true,usuariro:'admin',senha:0},
+        {Nome:'Larissa',SobreNome:'gomes lima',Cpf:0,Saldo:54280.78,Logado:true,usuariro:'esposa',senha:0},
+        {Nome:'Luiz Gustavo',SobreNome:'lima costa',Cpf:0,Saldo:54280.78,Logado:true,usuariro:'filho',senha:0}
+]
 }
 
 let usuario={
