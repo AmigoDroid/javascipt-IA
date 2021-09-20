d4b268e7bbdc2d5d8e10ded09550b14db148ef72
AmigoDroid <coltman05@gmail.com>
2021-09-19 23:51:30 -0300

    servidor consertado

--- routes.js
+++ routes.js
@@ -3,10 +3,10 @@ const routes = express.Router();
 
 
 let usuarios =[
-        {id:'0', Nome:'Luciano',SobreNome:'da silva costa',Cpf:0,Saldo:54280.78,usuariro:'admin',senha:0},
-        {id:'1',Nome:'Larissa',SobreNome:'gomes lima',Cpf:0,Saldo:10000000,usuariro:'Larissa123',senha:5824},
-        {id:'2',Nome:'Luiz Gustavo',SobreNome:'lima costa',Cpf:0,Saldo:54450280.78,usuariro:'Gustavo123',senha:5824},
-        {id:'3',Nome:'wheliton',SobreNome:'braz',Cpf:0,Saldo:18754200.51,usuariro:'braz01',senha:0000}
+        {id:'0', Nome:'Luciano',SobreNome:'da silva costa',Cpf:0,Saldo:54280.78,usuario:'admin',senha:0},
+        {id:'1',Nome:'Larissa',SobreNome:'gomes lima',Cpf:0,Saldo:10000000,usuario:'Larissa123',senha:5824},
+        {id:'2',Nome:'Luiz Gustavo',SobreNome:'lima costa',Cpf:0,Saldo:54450280.78,usuario:'Gustavo123',senha:5824},
+        {id:'3',Nome:'wheliton',SobreNome:'braz',Cpf:0,Saldo:18754200.51,usuario:'braz01',senha:0000}
         
 ]
 let usuario={
@@ -31,12 +31,14 @@ routes.get('/api/usuario',(req,res)=>{
     return res.json(usuarios);
 })
 routes.post('/adduser',(req,res)=>{
-    const dados = req.body;
+    const body = req.body;
     if(!body)
          return res.status(400).end();
 
-   // usuarios.push(dados)
-    return res(dados);
+    usuarios.push(body)
+    console.log(body);
+    return res.json({'resposta':true});
+    //
 })
 routes.delete('/usuario/del=:id',(req,res)=>{
     const id = req.params.id;
