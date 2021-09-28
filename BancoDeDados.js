d3141ab28f61d3dbd640c9fda8431211e35e311b
AmigoDroid <coltman05@gmail.com>
2021-09-28 20:08:56 -0300

    teste db TEOW

---
+++ BancoDeDados.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const Squelize = require('sequelize')
+const name_database='de7g3fm34n6p8v';
+const user='mkebhbglignllm';
+const pass='738e21132b9d802c4fd1ee94665b0c9a064d982947028ca55aef16ae4cd2258e';
+const porta=5432;
+const url='ec2-44-199-26-122.compute-1.amazonaws.com';
+const  tt ='postgres: // mkebhbglignllm : 738e21132b9d802c4fd1ee94665b0c9a064d982947028ca55aef16ae4cd2258e @ ec2-44-199-26-122.compute-1.amazonaws.com : 5432 / de7g3fm34n6p8';
+let teste='';
+const  db = new Squelize(name_database,user,pass,{host:url,port:process.envPORT,dialect:'mysql'
+})
+db.authenticate().then(function(){
+    console.log('conectado');
+    teste='conectado'
+
+}).catch(function(erro){
+    console.log('deu merda  '+erro)
+    teste='NÃO conectado '+erro;
+});
+const bancodedados = express.Router();
+
+
+
+bancodedados.get('/db',(req,res)=>{
+    console.log('ok');
+    res.send('deu certo: '+ teste)
+})
+
+
+
+
+
+
+
+module.exports = bancodedados;
--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const cors = require('cors');
 const boryparser = require('body-parser');
 
 const routes = require('./routes')
+const bancodedados = require('./BancoDeDados')
 //const bodyParser = require('body-parser');
 var porta = process.env.PORT || 8877;
 
@@ -13,6 +14,7 @@ app.use(boryparser.urlencoded({extended:false}));
 app.use(express.json());
 app.use(cors());
 app.use(routes);
+app.use(bancodedados);
 app.listen(porta,()=>{
     console.log('rodando na porta: '+porta);
  
