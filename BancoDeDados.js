29014d53ed18ef63bd8d2972aae13f711b86db00
AmigoDroid <coltman05@gmail.com>
2021-09-28 20:12:30 -0300

    teste

--- BancoDeDados.js
+++ BancoDeDados.js
@@ -7,7 +7,7 @@ const porta=5432;
 const url='ec2-44-199-26-122.compute-1.amazonaws.com';
 const  tt ='postgres: // mkebhbglignllm : 738e21132b9d802c4fd1ee94665b0c9a064d982947028ca55aef16ae4cd2258e @ ec2-44-199-26-122.compute-1.amazonaws.com : 5432 / de7g3fm34n6p8';
 let teste='';
-const  db = new Squelize(name_database,user,pass,{host:url,port:process.envPORT,dialect:'mysql'
+const  db = new Squelize(name_database,user,pass,{host:url,port:porta,dialect:'mysql'
 })
 db.authenticate().then(function(){
     console.log('conectado');
@@ -16,6 +16,7 @@ db.authenticate().then(function(){
 }).catch(function(erro){
     console.log('deu merda  '+erro)
     teste='NÃO conectado '+erro;
+
 });
 const bancodedados = express.Router();
 
