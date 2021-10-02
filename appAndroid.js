4726ed467fbf4b9b10dd836a84e360760c2e165b
AmigoDroid <coltman05@gmail.com>
2021-10-01 22:18:37 -0300

    add suport a app

---
+++ appAndroid.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const appAndroid = express.Router();
+
+
+const usuario = {
+    Nome:'Luciano',
+    SobreNome:'da silva costa'
+    ,Cpf:8476,Saldo:54280.78
+    ,usuario:'admin'
+    ,senha:'0'
+}
+function userInfo(user,pass){
+    
+
+}
+
+appAndroid.get('/app/login/:user/:pass',(req,res)=>{
+    let us = req.params.user;
+    let ps = req.params.pass;
+    userInfo(us,ps);
+
+    return res.json(usuario);
+
+})
+
+
+
+module.exports=appAndroid;
