f6103e35bf0b86a67d80b455ad351e422550322f
AmigoDroid <colteman05@gmail.com>
2021-11-21 20:09:21 -0200

    jfh

--- script/scripts.js
+++ script/scripts.js
@@ -82,7 +82,7 @@ async criartb(req,res){
 },
 async cadastrar(req,res){
     const dados = req.body;
-    config.sync(tabela);
+    this.criartb;
     await tabela.create(dados).then(()=>{
         return res.json({resposta:true,code:1});
     }).catch(()=>{
