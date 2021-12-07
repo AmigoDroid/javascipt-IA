8256b3d7038dada05a91894919f306ed0303c89f
AmigoDroid <colteman05@gmail.com>
2021-12-06 23:18:05 -0200

    update

--- login.js
+++ login.js
@@ -1,6 +1,24 @@
 const express = require('express');
 const login = express.Router();
 const script = require('./script/scripts');
+const wt = require('jsonwebtoken');
+const secret = 'zongi.sa-582426600'
+
+function altenticar(req,res,next){
+    const dados = req.body;
+    const token = dados.token;
+
+    wt.verify(token,secret , function(err,decoded){
+        if(err){
+           return res.json({resposta:false});
+        }else{
+            res.json({resposta:true});
+            next();
+        }
+    })
+    
+
+}
 
 login.post('/login',script.checar_user);
 
